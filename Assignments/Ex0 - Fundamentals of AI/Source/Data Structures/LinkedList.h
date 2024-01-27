e5c52deb03b28ef18774b0a8fe94435238289166
jacksonavery <58484091+jacksonavery@users.noreply.github.com>
2024-01-27 17:24:02 -0500

    failed pointer version

--- Assignments/Ex0 - Fundamentals of AI/Source/Data Structures/LinkedList.h
+++ Assignments/Ex0 - Fundamentals of AI/Source/Data Structures/LinkedList.h
@@ -1,6 +1,136 @@
-
+//#include <iostream>
 
 // LinkedList class should go in the "ufl_cap4053::fundamentals" namespace!
 namespace ufl_cap4053 { namespace fundamentals {
+
+
+	template<typename T> class LinkedList {
+	private:
+		struct ListNode {
+			T value;
+			ListNode* prev;
+			ListNode* next;
+
+			ListNode() {}
+			ListNode(T val, ListNode* pr = nullptr, ListNode* nx = nullptr) {
+				value = val;
+				prev = pr;
+				next = nx;
+			}
+		};
+		
+		ListNode* startptr;
+		ListNode* endptr;
+
+
+	public:
+		struct Iterator {
+		public:
+			ListNode* curr;
+
+			Iterator() { curr = startptr; }
+			Iterator(ListNode* node) { curr = node; }
+
+			T operator*() const {
+				return curr->value;
+			}
+			Iterator& operator++() {
+				curr = curr->next;
+				return *this;
+			}
+
+			bool operator==(Iterator const& rhs) {
+				return (rhs.curr == curr);
+			}
+			bool operator!=(Iterator const& rhs) {
+				return !(rhs.curr == curr);
+			}
+
+		};
+
+		LinkedList() { startptr = endptr = nullptr; }
+
+		Iterator begin() const { return Iterator(startptr); }
+		Iterator end() const { return Iterator(nullptr); }
+		bool isEmpty() const { return startptr == nullptr; }
+		T getFront() const { return startptr->value; }
+		T getBack() const { return endptr->value; }
+
+		void enqueue(T element) {
+			ListNode* node = new ListNode(element, endptr, nullptr);
+			// attach to end of list
+			if (endptr != nullptr) {
+				endptr->next = node;
+			}
+			// set as new end
+			endptr = node;
+
+			// set as start if only node in list
+			if (startptr == nullptr) {
+				startptr = node;
+			}
+
+		}
+		//  all removals pipe through the same function. this is marginally slower
+		// but it means i only have to implement removal (and associated edge cases) once
+		void dequeue() {
+			removebyit(startptr);
+		}
+		void pop() {
+			if (endptr != nullptr) {
+				removebyit(endptr);
+			}
+		}
+		// yeah im wrong for this
+		void clear() {
+			while (!isEmpty()) {
+				pop();
+			}
+		}
+		// not asked for but makes contains()/remove() a bit easier
+		Iterator find(T element) const {
+			Iterator it(startptr);
+			while (it != nullptr) {
+				if (*it == element) { return it; }
+				++it;
+			}
+			return nullptr;
+		}
+		bool contains(T element) const {
+			return (find(element) != nullptr);
+		}
+		void remove(T element) {
+			Iterator at = find(element);
+			
+			removebyit(at);
+		}
+		// the aforementioned true removal function
+		void removebyit(Iterator at) {
+			// easy escape case
+			if (at == nullptr) { return; }
+
+			// test if first element
+			if (at.curr->prev == nullptr) {
+				startptr = at.curr->next;
+			}
+			else {
+				at.curr->prev->next = at.curr->next;
+			}
+			// test if last element
+			if (at.curr->next == nullptr) {
+				endptr = at.curr->prev;
+			}
+			else {
+				at.curr->next->prev = at.curr->prev;
+			}
+
+			delete at.curr;
+		}
+
+		//void printState() {
+		//	std::cout << "\n=Startpointer exists: " << (startptr != nullptr) << "\n=Endpointer exists: " << (startptr != nullptr);
+		//}
+	};
+
 
 } }  // namespace ufl_cap4053::fundamentals
--- Assignments/Ex0 - Fundamentals of AI/Source/Data Structures/TreeNode.h
+++ Assignments/Ex0 - Fundamentals of AI/Source/Data Structures/TreeNode.h
@@ -1,6 +1,89 @@
+#include "../Data Structures/LinkedList.h"
 
 
 // TreeNode class should go in the "ufl_cap4053::fundamentals" namespace!
 namespace ufl_cap4053 { namespace fundamentals {
+
+
+	template<typename T> class TreeNode {
+	private:
+		T val;
+		TreeNode** children;
+		size_t childCount = 0;
+
+	public:
+		TreeNode() { val = nullptr; };
+		TreeNode(T element) { val = element; }
+
+		const T& getData() const {
+			return val;
+		}
+		size_t getChildCount() const {
+			return childCount;
+		}
+		TreeNode<T>* getChild(size_t index) {
+			return children[index];
+		}
+		TreeNode<T>* getChild(size_t index) const {
+			return children[index];
+		}
+		void addChild(TreeNode<T>* child) {
+			TreeNode<T>** newChildren = new TreeNode<T>[childCount+1];
+			// im sure you could memcopy this but im a little rusty
+			for (int i = 0; i < childCount; i++) {
+				newChildren[i] = children[i];
+			}
+			newChildren[childCount] = child;
+			
+			delete[] children;
+			children = newChildren;
+			
+			childCount++;
+		}
+		TreeNode<T>* removeChild(size_t index) {
+			TreeNode<T>** newChildren = new TreeNode<T>[childCount - 1];
+			// j runs alongside but skips over the removed element
+			int j = 0;
+			for (int i = 0; i < childCount; i++) {
+				if (i == index)
+					j++;
+				newChildren[i] = children[j];
+				j++;
+			}
+			TreeNode<T>* ret = &children[index];
+
+			delete[] children;
+			children = newChildren;
+
+			childCount--;
+
+			return ret;
+		}
+		//takes advantage of the linkedlist class
+		void breadthFirstTraverse(void (*dataFunction)(const T)) const {
+			LinkedList<TreeNode*> nodeQueue;
+			for (auto itr = nodeQueue.begin(); itr != nodeQueue.end(); ++itr) {
+				TreeNode* current = (itr.curr->value);
+				for (int i = 0; i < current->getChildCount(); i++) {
+					nodeQueue.enqueue(current->getChild(i));
+				}
+				dataFunction(current->val);
+			}
+
+		}
+		void preOrderTraverse(void (*dataFunction)(const T)) const {
+			dataFunction(val);
+			for (int i = 0; i < childCount; i++) {
+				children[i]->preOrderTraverse(dataFunction);
+			}
+		}
+		void postOrderTraverse(void (*dataFunction)(const T)) const {
+			for (int i = 0; i < childCount; i++) {
+				children[i]->postOrderTraverse(dataFunction);
+			}
+			dataFunction(val);
+		}
+	};
+
 
 }}  // namespace ufl_cap4053::fundamentals
